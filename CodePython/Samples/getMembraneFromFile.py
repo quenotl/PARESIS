9df9cb4618019662c8f1b290e2d11b5d2b49400f
quenot <laurene.quenot@inserm.fr>
2021-04-28 17:02:09 +0200

    membrane stitching for creating bigger ones

--- CodePython/Samples/getMembraneFromFile.py
+++ CodePython/Samples/getMembraneFromFile.py
@@ -39,40 +39,71 @@ def getMembraneFromFile(myMembraneFile,studyDimensions,studyPixelSize, oversamp,
 
 
 def getMembraneSegmentedFromFile(sample,dimX,dimY,pixSize,overSamp, pointNum):
-    margin=int(np.ceil(10*sample.myMeanSphereRadius/pixSize))
+    margin=int(np.ceil(10*sample.myMeanSphereRadius/pixSize)) #in pix
     margin2=int(np.floor(margin/2))
+    
     if sample.myMembraneFile.split('/')[-1]=='CuSn.txt':
-        corrFactor=sample.myMeanSphereRadius/6
-        membraneSizeinFiley=int(np.floor(8178))*corrFactor 
-        membraneSizeinFilex=int(np.floor(9813))*corrFactor 
+        corrFactor=sample.myMeanSphereRadius/6 #to adapt the scale to the mean sphere radius desired
+        membraneSizeinFilex=int(np.floor(8102))*corrFactor+sample.myMeanSphereRadius
+        membraneSizeinFiley=int(np.floor(9740))*corrFactor+sample.myMeanSphereRadius
     else: 
         raise Exception("Enter segmented membrane size ")
     
     with open(sample.myMembraneFile) as file:
         parameters=json.load(file)
-        
+    parameters=np.asarray(parameters) #change list to numpy array 2D [nsphere, values] 
+    parameters=parameters*corrFactor  #to adapt the scale to the mean sphere radius desired
+    paramx=parameters[:,1]
+    paramy=parameters[:,0]
+    parameters[:,1]+=membraneSizeinFilex/2 #place origin on top left instead of middle
+    parameters[:,0]+=membraneSizeinFiley/2
+    paramx=parameters[:,1]
+    paramy=parameters[:,0]
     
+    parameters0=np.copy(parameters)
+        
+    DiffFileExpX=int(np.floor((membraneSizeinFilex/pixSize)-dimX))
+    while DiffFileExpX<0: #as long as the membrane in file is too small, expand it
+        print("segmented membrane too small: proceeding with stitching along x")
+        parametersStitchX=np.copy(parameters0)
+        parametersStitchX[:,1]+=membraneSizeinFilex
+        parameters=np.concatenate((parameters, parametersStitchX), axis=0)
+        membraneSizeinFilex+=membraneSizeinFilex
+        DiffFileExpX=int(np.floor((membraneSizeinFilex/pixSize)-dimX))-2*margin
+        
+    parameters0=np.copy(parameters)
+    
+    DiffFileExpY=int(np.floor((membraneSizeinFiley/pixSize)-dimY))-2*margin
+    while DiffFileExpY<0:
+        print("segmented membrane too small: proceeding with stitching along y")
+        parametersStitchY=np.copy(parameters0)
+        parametersStitchY[:,0]+=membraneSizeinFiley
+        parameters=np.concatenate((parameters, parametersStitchY), axis=0)
+        membraneSizeinFiley+=membraneSizeinFiley
+        DiffFileExpY=int(np.floor((membraneSizeinFiley/pixSize)-dimY))-2*margin
+        
     membrane=np.zeros((dimX+2*margin,dimY+2*margin))
-    Nsphere=len(parameters)
+    Nsphere,_=parameters.shape
     
     Offsetx=0
     Offsety=0
     offsetCorr=0
     
-    for nmem in range(sample.myNbOfLayers):
-        if pointNum!=0 or nmem!=0:
-            maxOffsetx=int(np.floor((membraneSizeinFilex/pixSize)/2-dimX/2))-margin
-            maxOffsety=int(np.floor((membraneSizeinFiley/pixSize)/2-dimY/2))-margin
-            Offsetx=np.random.random_integers(-maxOffsetx,maxOffsetx)*pixSize
-            Offsety=np.random.random_integers(-maxOffsety,maxOffsety)*pixSize
+    for nlayer in range(sample.myNbOfLayers):
+        # if pointNum!=0 or nmem!=0:
+        maxOffsetx=int(np.floor((membraneSizeinFilex/pixSize)-dimX))
+        maxOffsety=int(np.floor((membraneSizeinFiley/pixSize)-dimY))
+        Offsetx=np.random.randint(margin2,maxOffsetx-margin2)
+        Offsety=np.random.randint(margin2,maxOffsety-margin2)
         
         for i in range(Nsphere):
-            radFloat=parameters[i][2]*corrFactor/pixSize
+            radFloat=parameters[i,2]/pixSize
             radInt=int(np.floor(radFloat))+1
-            x=int(np.round(parameters[i][1]*corrFactor/pixSize+dimX/2+margin+offsetCorr+Offsety/pixSize))
-            y=int(np.round(parameters[i][0]*corrFactor/pixSize+dimY/2+margin+offsetCorr+Offsetx/pixSize))
-            xfloat=parameters[i][1]*corrFactor/pixSize+dimX/2+margin+Offsety/pixSize+offsetCorr
-            yfloat=parameters[i][0]*corrFactor/pixSize+dimY/2+margin+Offsetx/pixSize+offsetCorr
+            xdata=parameters[i,0]
+            xfloat=parameters[i,1]/pixSize-Offsetx+offsetCorr
+            yfloat=parameters[i,0]/pixSize-Offsety+offsetCorr
+            x=int(np.round(xfloat))
+            y=int(np.round(yfloat))
             if margin2<x<dimX+margin+margin2 and margin2<y<dimY+margin+margin2:
     #            print(x,y,radFloat)
                 for ii in range(-radInt,radInt):
@@ -81,10 +112,8 @@ def getMembraneSegmentedFromFile(sample,dimX,dimY,pixSize,overSamp, pointNum):
                         if dist<radFloat:
                             membrane[x+ii,y+jj]+=2*np.sqrt(radFloat**2-dist**2)
                             
-    membrane=membrane[margin:margin+dimX,margin:margin+dimY]
+    membrane=membrane[margin:-margin,margin:-margin]
     return membrane*pixSize*1e-6
-
-
 
     
 if __name__ == "__main__":
@@ -101,17 +130,17 @@ if __name__ == "__main__":
     imageMargins=10
     overSamp=4
     Nx=200 #Detector size
-    Ny=200
+    Ny=2500
     dimX=int((Nx+2*imageMargins)*overSamp)
     dimY=int((Ny+2*imageMargins)*overSamp)
     dist_source_sample=0.1 #in m
     dist_sample_detector=0.5 #in m
-    detector_pixel_size=24 #in um
+    detector_pixel_size=75 #in um
     magnification=(dist_sample_detector+dist_source_sample)/dist_source_sample
     pixSize=detector_pixel_size/overSamp/magnification
-    storingFolder='Membranes/CuSn_dim'+str(Nx)+'x'+str(Ny)+'_oversamp'+str(overSamp)+'_margin'+str(imageMargins)+'/'
     
     membrane=Membrane()
+    storingFolder='Membranes/CuSn_dim'+str(Nx)+'x'+str(Ny)+'_oversamp'+str(overSamp)+'_radius'+str(imageMargins)+'_margin'+str(membrane.myMeanSphereRadius)+'/'
     
     maxx=(9813/6*membrane.myMeanSphereRadius)
     maxy=(8178/6*membrane.myMeanSphereRadius)
@@ -120,14 +149,19 @@ if __name__ == "__main__":
 
     print("Maximum size of the membrane you can generate with those parameters: %d x %d um" %(maxx ,maxy) )
     print("The one you are trying to create is %d x %d um" %(dimX*pixSize, dimY*pixSize))
-    if dimX>maxDx or dimY>maxDy:
-        raise Exception("The segmented membrane is too small for the one you are trying to generate. (You can try increasing the sphere mean radius)")
+    # if dimX>maxDx or dimY>maxDy:
+    #     raise Exception("The segmented membrane is too small for the one you are trying to generate. (You can try increasing the sphere mean radius)")
     
-    os.mkdir(storingFolder)
+    if not os.path.exists(storingFolder):
+        os.mkdir(storingFolder)
     
     for pointNum in range (number_of_positions):
         print("\nNumber of the position currently processed ", pointNum)
-        membraneThickness=getMembraneSegmentedFromFile(membrane,dimX,dimY,pixSize,overSamp, pointNum)
+        membraneThickness=getMembraneSegmentedFromFile(membrane,dimX,dimY,pixSize,overSamp, pointNum) 
+        plt.figure()
+        plt.imshow(membraneThickness)
+        plt.colorbar()
+        plt.show()
         txtPoint = '%2.2d' % pointNum
         saveEdf(membraneThickness, storingFolder+txtPoint+'.edf')
     
